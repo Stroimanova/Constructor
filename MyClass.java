2cc8a387bdc299359c7fea6d5cae0d463a8b0ae0
unknown <simonastroimanova@hotmail.com>
2023-12-14 18:14:48 +0100

    first java program

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+package projkkk;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Vehicle v1 = new Vehicle(); // Using default color "Blue"
+        Vehicle v2 = new Vehicle("Red"); // Setting color to "Red"
+        
+		System.out.println("Vehicle color: + blue"); // Prints "Vehicle color: "Blue"
+		System.out.println("Vehicle color: + red"); // Prints "Vehicle color: "Red"
+
+
+	}
+
+}
---
+++ Vehicle.java
@@ -0,0 +1,16 @@
+package projkkk;
+
+public class Vehicle {
+	private String color;
+	
+	// Constructor with a default color
+	Vehicle() {
+		color = "Blue";
+		// Set a default color, e.g., "Blue"
+	}
+	// Constructor with a parametar to set the color
+	Vehicle(String a) {
+		color = a;
+	}
+
+}
